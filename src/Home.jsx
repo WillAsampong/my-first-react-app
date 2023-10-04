5555e17811fb5582aad1778a81c41909eb02a4bf
Will <asampongg377@gmail.com>
2023-10-04 18:43:25 +0000

    Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,10 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
 
-const url = 'https://jsonplaceholder.typicode.com/users';
-const [users, setUsers] = useState([]);
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const [users, setUsers] = useState([]);
+    const [search, setSearch] = useState('');
+    // console.log(search);
 
     const fetchUsers = async () => {   
         try {
@@ -24,42 +26,47 @@ const [users, setUsers] = useState([]);
         fetchUsers()
     }, []);
 
+    const handleChange = (e) => {
+        setSearch(e.target.value);       
+    }
 
   return (
     <div className="home">
         <div className="p-12 h-full">
             <div className="heading mb-6">
                 <h1 className="text-3xl font-bold text-mBlue">List Of Users</h1>
-                <div className="underline border-2 w-20 border-mBlue"></div>
+                <div className="underline border-2 w-28 border-mBlue"></div>
             </div>
             <div className="table  h-2/3 p-8 pl-0">
                 <div className="search-heading flex">
-                    <FontAwesomeIcon icon={faUser} className="text-3xl text-mBlue mr-8"/>
+                    <FontAwesomeIcon icon={faUser} className="text-3xl text-mBlue mr-4"/>
                     <h3 className="text-2xl font-bold text-mBlue mb-4">Search User</h3>
                 </div>
                 <div className="search-input h-10 w-full mb-8">
-                    <input type="text" placeholder="search user..." className="h-full mr-6 border outline-mBlue w-3/4 p-6"/>
+                    <input type="text" placeholder="search user..." className="h-full mr-6 border outline-mBlue w-3/4 p-6" onChange={handleChange} value={search}/>
                     <FontAwesomeIcon icon={faMagnifyingGlass} className="text-mBlue text-lg"/>
                 </div>
-                <table className='p-12'>
-                    <thead className="p-8 h-8 bg-mBlue text-white text-left">
+                <table className='table-auto p-12 border-b border-mBlue shadow-lg'>
+                    <thead className="bg-mBlue text-white text-left">
                         <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
+                            <th className="p-6">Name</th>
+                            <th className="p-6">Email</th>
+                            <th className="p-6">Phone</th>
                         </tr>
                     </thead>
                     <tbody className="p-8">
-                        {
-                            users.map((user) => (
-                                <tr key={user.id} className="p-8">
-                                    <td>{user.name}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.phone}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
+                    {users
+                    .filter((item) =>
+                        search === '' ? item : item.name.toLowerCase().includes(search)
+                    )
+                    .map((user) => (
+                        <tr key={user.id} className="h-8">
+                        <td className="p-6 border-b border-mBlue">{user.name}</td>
+                        <td className="p-6 border-b border-mBlue">{user.email}</td>
+                        <td className="p-6 border-b border-mBlue">{user.phone}</td>
+                        </tr>
+                    ))}
+                </tbody>
                 </table>
             </div>
         </div>
